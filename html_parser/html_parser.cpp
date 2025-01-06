ce9c2c06cf047dac55d8ebdbfd046db71831fc5a
Andrii <kaptur.andrey22001@gmail.com>
2025-01-06 14:43:32 +0200

    1

--- html_parser/html_parser.cpp
+++ html_parser/html_parser.cpp
@@ -55,6 +55,20 @@ std::string HTMLTagParser::parseEncoding(const std::string& html_content) {
     return "";
 }
 
+std::vector<std::pair<std::string, std::string>> HTMLTagParser::parseButtons(const std::string& html_content) {
+    std::vector<std::pair<std::string, std::string>> buttons;
+    std::smatch match;
+    auto button_begin = std::sregex_iterator(html_content.begin(), html_content.end(), button_regex);
+    auto button_end = std::sregex_iterator();
+
+    for (auto it = button_begin; it != button_end; ++it) {
+        std::string button_class = (*it)[1];  // class
+        std::string button_text = (*it)[2];   // текст на кнопці
+        buttons.push_back({ button_class, button_text });
+    }
+    return buttons;
+}
+
 HTMLParser::HTMLParser(const char* file_name) {
     html_file.open(file_name);
     if (!html_file) {
@@ -79,11 +93,17 @@ void HTMLParser::getHTMLData() {
     std::string body = html_tags.parseBody(html_content);
     std::string lang = html_tags.parseLang(html_content);
     std::string encoding = html_tags.parseEncoding(html_content);
+    std::vector<std::pair<std::string, std::string>> buttons = html_tags.parseButtons(html_content);
 
     std::cout << "Версія HTML: " << version << "\n";
     std::cout << "Мова сторінки: " << lang << "\n";
     std::cout << "Кодування сторінки: " << encoding << "\n";
     std::cout << "Назва сторінки: " << title << "\n";
     std::cout << "Body код: \n(" << body << ");\n";
+
+    std::cout << "Кнопки на сторінці:\n";
+    for (const auto& button : buttons) {
+        std::cout << "Клас: " << button.first << ", Текст: " << button.second << "\n";
+    }
 
 }
--- html_parser/html_parser.h
+++ html_parser/html_parser.h
@@ -14,12 +14,14 @@ struct HTMLTagParser {
     std::regex lang_regex{ "<html.*?lang=\"([^\"]+)\"" };
     std::regex encoding_regex{ "<meta.*?charset=\"([^\"]+)\"" };
     std::regex body_regex{ R"(<body[^>]*>([\s\S]*?)</body>)" };
+    std::regex button_regex{ R"(<button[^>]*class=\"([^\"]*)\"[^>]*>(.*?)</button>)" };
 
 
     std::string parseTitle(const std::string& html_content);
     std::string parseLang(const std::string& html_content);
     std::string parseEncoding(const std::string& html_content);
     std::string parseBody(const std::string& html_content);
+    std::vector<std::pair<std::string, std::string>> parseButtons(const std::string& html_content);
 };
 
 class HTMLParser {
